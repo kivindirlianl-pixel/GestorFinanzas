d4325b122f03dd554ba9a951a219d519219fb4b0
kivindirlianl-pixel <kivindirlianl@gmail.com>
2026-06-23 21:49:36 -0500

    actualizacion 1.0

--- js/app.js
+++ js/app.js
@@ -422,3 +422,12 @@ configurarFormatoMonedaEnInput('movimientoMonto');
 configurarFormatoMonedaEnInput('compraSugerido');
 configurarFormatoMonedaEnInput('compraReal');
 configurarFormatoMonedaEnInput('hormigaMonto');
+
+// Service Worker offline apuntando a la raíz
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./sw.js')
+            .then(reg => console.log('Service Worker registrado con éxito', reg))
+            .catch(err => console.log('Error al registrar el Service Worker', err));
+    });
+}
